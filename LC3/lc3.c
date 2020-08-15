3eb7ef1ffe8253da433a320586415e2d8157e40c
suraj <i@surajsau.in>
2020-08-15 16:52:19 +0530

    :barber: Added ADD opcode logic

--- LC3/lc3.c
+++ LC3/lc3.c
@@ -124,6 +124,28 @@ int main(int argc, const char * argv[]) {
         
         switch (op) {
             case OP_ADD:
+                {
+                    /* destination register (DR) */
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    
+                    /* first operand (SR1) */
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    
+                    /* whether immediate mode or register mode */
+                    uint16_t imm_flag = (instr >> 5) & 0x1;
+                    
+                    if(imm_flag) {
+                        /* imm5, immediate value passed in ADD for immediate mode */
+                        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                        reg[r0] = reg[r1] + imm5;
+                    } else {
+                        /* second operand (SR2) incase of register mode */
+                        uint16_t r2 = instr & 0x7;
+                        reg[r0] = reg[r1] + reg[r2];
+                    }
+                    
+                    update_flags(r0);
+                }
                 break;
             case OP_AND:
                 break;
