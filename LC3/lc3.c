2dabb0e6c9e3441b382e41e9cdb4a2bd9b14f7cf
suraj <i@surajsau.in>
2020-08-15 16:46:19 +0530

    :bento: Added memory, registers, opcodes and condition flags

--- LC3/lc3.c
+++ LC3/lc3.c
@@ -16,6 +16,67 @@
 #include <sys/termios.h>
 #include <sys/mman.h>
 
+// Memory
+/* 2^16 = 65536 memory allocation */
+uint16_t memory[UINT16_MAX];
+
+// Register
+/*
+    - 8 general purpose registers (R0-R7): used to perform program calculations
+    - 1 program counter register (PC): store address of next operation to be performed
+    - 1 condition flag register (COND): tells information about the previous operation performed
+ */
+enum {
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, /* program counter register */
+    R_COND, /* condition flag register */
+    R_COUNT
+};
+
+uint16_t reg[R_COUNT];
+
+// Instruction Set
+/*
+    LC-3 has 16 opcodes
+ */
+enum {
+    OP_BR = 0, /* branch */
+    OP_ADD, /* add */
+    OP_LD,  /* load */
+    OP_ST,  /* store */
+    OP_JSR, /* jump register */
+    OP_AND, /* bitwise and */
+    OP_LDR, /* load register */
+    OP_STR, /* store register */
+    OP_RTI, /* unused */
+    OP_NOT, /* bitwise not */
+    OP_LDI, /* load indirect */
+    OP_STI, /* store indirect */
+    OP_JMP, /* jump */
+    OP_RES, /* reserved (unused) */
+    OP_LEA, /* load effective address */
+    OP_TRAP /* execute trap */
+};
+
+// Condition flags
+/*
+    This LC-3 will be having three condition flags which is stored in R_COND.
+    This value tells us about the result of the most recent calculation and is
+    useful to check logical conditions like if(x > 0) { .. }
+ */
+enum {
+    FL_POS = 1 << 0,    // positive
+    FL_ZRO = 1 << 1,    // zero
+    FL_NEG = 1 << 2,     // negative
+};
+
 int main(int argc, const char * argv[]) {
     return 0;
 }
