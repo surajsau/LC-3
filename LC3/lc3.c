27abf8df7e5f89260eaa0308d44c492e4d3d752d
suraj <i@surajsau.in>
2020-08-15 16:43:05 +0530

    :star2: Started with LC-3 tutorial. Added standard headers

---
+++ LC3/lc3.c
@@ -0,0 +1,21 @@
+//
+//  LC3
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <signal.h>
+/* unix */
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
