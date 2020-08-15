c39b59d77dca7b43248d7f784e148eb5b5e678a5
suraj <i@surajsau.in>
2020-08-15 16:50:50 +0530

    :sparkles: Added opcode processing switch statement in main(). Also initialised starting memory location for registers

--- LC3/lc3.c
+++ LC3/lc3.c
@@ -102,5 +102,61 @@ void update_flags(uint16_t r) {
 }
 
 int main(int argc, const char * argv[]) {
+    
+    /*
+        load arguments
+     */
+    if(argc < 2) {
+        printf("main [image-file1] .. \n");
+        exit(2);
+    }
+    
+    /*
+        set the starting position.
+     */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+    
+    int running = 1;
+    while(running) {
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+        
+        switch (op) {
+            case OP_ADD:
+                break;
+            case OP_AND:
+                break;
+            case OP_NOT:
+                break;
+            case OP_BR:
+                break;
+            case OP_JMP:
+                break;
+            case OP_JSR:
+                break;
+            case OP_LD:
+                break;
+            case OP_LDI:
+                break;
+            case OP_LDR:
+                break;
+            case OP_LEA:
+                break;
+            case OP_ST:
+                break;
+            case OP_STI:
+                break;
+            case OP_STR:
+                break;
+            case OP_TRAP:
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                break;
+        }
+    }
+    
     return 0;
 }
